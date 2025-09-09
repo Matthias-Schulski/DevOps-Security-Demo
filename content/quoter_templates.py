84bfa8516709fa1f6107c7341dfcd11560bac727
Matthias-Schulski <Matthias@schulski.nl>
2025-09-09 12:24:26 +0200

    Update quoter_templates.py

--- content/quoter_templates.py
+++ content/quoter_templates.py
@@ -75,7 +75,7 @@ def page(content,user_id,title,error=None):
 
 <header>
   <div class="title">
-    <a class="home" href="/">65656</a>
+    <a class="home" href="/">Matthias</a>
     {links}
   </div>
 </header>
