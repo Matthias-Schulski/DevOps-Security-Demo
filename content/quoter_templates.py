96a2943ac3fbb29434f6d5884ecec38a0e5a6ce6
Matthias-Schulski <Matthias@schulski.nl>
2025-09-09 15:49:05 +0200

    Update quoter_templates.py

--- content/quoter_templates.py
+++ content/quoter_templates.py
@@ -75,7 +75,7 @@ def page(content,user_id,title,error=None):
 
 <header>
   <div class="title">
-    <a class="home" href="/">Matthias</a>
+    <a class="home" href="/">Matthias2</a>
     {links}
   </div>
 </header>
